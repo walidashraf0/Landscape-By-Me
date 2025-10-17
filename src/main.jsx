d5d73a34bfa9a89dfc07511f480359a41d017d85
Walid Ashraf <walidashraf050@gmail.com>
2025-10-17 18:35:47 +0300

    fix error sitemap 8

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <HelmetProvider>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </HelmetProvider>
   </StrictMode>
 );
